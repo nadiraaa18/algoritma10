5867568f2e09819d14e79bb9151e0a7079b565d4
nadiraaa18 <amninadira71@gmail.com>
2024-12-30 09:53:28 +0800

    Add files via upload

---
+++ Algori1101.py
@@ -0,0 +1,29 @@
+print("perulangan 1")
+for i in range(8):
+    print (i)
+
+print("perulangan 2")
+for i in range(1,6):
+    print (i)
+
+
+print("perulangan dengan step")
+for i in range(1,20,3):
+    print (i)
+
+numbers = [10, 20, 30, 40, 50]
+total = 0
+
+for num in numbers:
+    total += num
+
+print("Total nilai:", total)
+
+
+for i in [1, 2, 3, 4, 5]:
+ print ("Inipengulangan ke - ", i)
+
+
+# pengulangan sebanyak 8 kali
+for i in ["Rawon", "Nasi Kuning", "Soto Madura", "Kupat Tahu", "Kerak telur", "Rendang Batoko", "Pempek Selam", "Ayam Betutu"] :
+ print (i, " adalah masakan khas nusantara …")
